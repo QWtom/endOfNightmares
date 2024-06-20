e4ff6001e162543a82a2e63624f27079da797807
tom <qw.tom2@gmail.com>
2024-06-20 14:18:26 +0900

    upd

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://qwtom.github.io'
+    site: 'https://QWtom.github.io'
   })
